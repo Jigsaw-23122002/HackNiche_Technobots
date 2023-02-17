fa1ec40f7b7042211b2d8803f1b21b85debeeaf7
Harsh <harshnag23@gmail.com>
2023-02-17 11:55:53 +0530

    Files uploaded on IPFS and QR generated opens the PDF

--- app/pages/upload.js
+++ app/pages/upload.js
@@ -1,42 +1,55 @@
+import axios from "axios";
 import { useState } from "react";
 import SVG from "react-inlinesvg";
 import { useDropzone } from "react-dropzone";
-import axios from "axios";
-import { convertPdfToImages } from "../Jsx";
+import { Web3Storage } from "web3.storage";
 
 let files = [];
+let document_cid;
+let filename;
 
-export default function Upload() {
+export default function Home() {
   const { getRootProps, getInputProps } = useDropzone({});
+  const [input, setInput] = useState(null);
   const [response, setResponse] = useState(null);
 
-  function selectFile(e) {
+  async function selectFile(e) {
     files = [];
     files.push(e.target.files[0]);
-    getQrcode();
   }
 
-  function generateUuid() {
-    return crypto.randomUUID();
-  }
+  const uploadDocument = async () => {
+    const token = process.env.NEXT_PUBLIC_WEB3STORAGE_API_TOKEN;
+    if (!token) {
+      return console.error(
+        "A token is needed. You can create one on https://web3.storage"
+      );
+    }
+    const storage = new Web3Storage({ token });
+    document_cid = await storage.put(files);
+    filename = files[0].name;
+    await getQrcode();
+  };
 
   const getQrcode = async () => {
     try {
       const res = await axios.get("api/qrcode/", {
-        params: {
-          input: generateUuid(),
-        },
+        params: { input: `https://${document_cid}.ipfs.w3s.link/${filename}` },
       });
       setResponse(res.data);
-      files[0] = await convertPdfToImages(files[0], response);
-      console.log(files);
+      // Store the generated QR svg and cid into the contract into the structure.
     } catch (error) {
       console.log(error);
     }
   };
-
   return (
-    <div>
+    <div className="flex flex-col relative bg-grey font-mono items-center min-h-screen border-t-2 border-active">
+      <h1 className="text-6xl font-bold text-primary mt-20">
+        QR Code <span className="text-active">Generator</span>
+      </h1>
+      <h2 className="text-active text-2xl mt-6">
+        Generate a QR Code for sharing your content.
+      </h2>
       <div {...getRootProps({ className: "dropzone" })}>
         <div className="flex items-center justify-center w-full my-8">
           <label
@@ -61,7 +74,7 @@ export default function Upload() {
               </svg>
               <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
                 <span className="font-semibold">Click to upload</span> the
-                Memorandom of Association (MoA) of the organization
+                requested document
               </p>
               <p className="text-xs text-gray-500 dark:text-gray-400">
                 The files should be of PDF format ( size less than 100 KB )
@@ -77,6 +90,12 @@ export default function Upload() {
           </label>
         </div>
       </div>
+      <button
+        className="mt-6 p-4 bg-active hover:opacity-90 rounded text-primary font-bold inline-flex"
+        onClick={() => uploadDocument()}
+      >
+        Generate QR Code
+      </button>
       {response && (
         <div className="mt-10 bg-active">
           <SVG src={response} />
@@ -91,3 +110,57 @@ export default function Upload() {
     </div>
   );
 }
+// import { useState } from "react";
+// import SVG from "react-inlinesvg";
+//
+// import axios from "axios";
+// import { convertPdfToImages } from "../Jsx";
+
+// let files = [];
+
+// export default function Upload() {
+//   const { getRootProps, getInputProps } = useDropzone({});
+//   const [response, setResponse] = useState(null);
+
+//   function selectFile(e) {
+//     files = [];
+//     files.push(e.target.files[0]);
+//     getQrcode();
+//   }
+
+//   function generateUuid() {
+//     return crypto.randomUUID();
+//   }
+
+//   const getQrcode = async () => {
+//     try {
+//       const res = await axios.get("api/qrcode/", {
+//         params: {
+//           input: generateUuid(),
+//         },
+//       });
+//       setResponse(res.data);
+//       files[0] = await convertPdfToImages(files[0], response);
+//       console.log(files);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   return (
+//     <div>
+
+//       {response && (
+//         <div className="mt-10 bg-active">
+//           <SVG src={response} />
+//           <button
+//             className="w-full text-primary text-base p-1"
+//             onClick={() => downloadQrcode()}
+//           >
+//             Download
+//           </button>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
