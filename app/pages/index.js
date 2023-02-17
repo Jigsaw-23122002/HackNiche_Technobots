4a5f75feaf3bce45af97488c101d73d0a4ab226f
Harsh <harshnag23@gmail.com>
2023-02-17 13:18:33 +0530

    Wallet connection done

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,9 +1,61 @@
 import { Contract, providers, utils } from "ethers";
+import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
-//https://${req.body.cid}.ipfs.w3s.link/${req.body.filename}
+import { CONTRACT_ADDRESS, CONTRACT_ABI } from "../constants";
+
 export default function Home() {
+  const web3ModalRef = useRef();
   const [walletConnected, setWalletConnected] = useState(false);
 
-  return <div></div>;
+  const getProviderOrSigner = async (needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+    const { chainId } = await web3Provider.getNetwork();
+
+    if (chainId !== 44787) {
+      window.alert("Change the network to Celo");
+      throw new Error("Change network to Celo");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, []);
+
+  return (
+    <div>
+      {!walletConnected ? (
+        <>
+          <p>Wallet not connected</p>
+        </>
+      ) : (
+        <>
+          <p>Wallet Connected</p>
+        </>
+      )}
+    </div>
+  );
 }
